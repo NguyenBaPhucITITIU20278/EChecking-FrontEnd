42e2b6aeab7a5900e9787d332584dd4db32a8aff
Saviorrrrrrr <nguyentrinhnhatminh113@gmail.com>
2024-05-10 23:14:41 +0700

    change font for all pages

--- src/pages/DashboardStudent.jsx
+++ src/pages/DashboardStudent.jsx
@@ -16,14 +16,14 @@ const DashboardStudent = () => {
                 <div className="relative z-10 flex flex-col sm:flex-row justify-center items-center h-screen">
                     <button className="border-2 border-gray-400 relative w-144 h-100 sm:w-96 sm:h-64 bg-white shadow-lg p-8 mr-32 sm:mr-16 mt-10 sm:mt-50 rounded-2xl hover:scale-105 transition duration-300 ">
                         <img src={Checkattendance} alt="Check Attendance" className="pl-2 absolute top-14 left-32 w-28 h-26" />
-                        <h1 className="text-2xl font-semibold mt-32 ml-10 mr-10 text-gray-700"> Check attendance</h1>
+                        <h1 className="text-2xl font-montserrat font-semibold mt-32 ml-10 mr-10 text-gray-700"> Check attendance</h1>
                     </button>
                     <button className="border-2 border-gray-400 relative w-144 h-100 sm:w-96 sm:h-64 bg-white shadow-lg p-8 mt-10 sm:mt-50 rounded-2xl hover:scale-105 transition duration-300 ml-32 sm:ml-16">
                         <img src={Setting} alt="Setting" className="pl-2 absolute top-14 left-32 w-28 h-226" />
-                        <h1 className="text-2xl font-semibold mt-32 ml-10 mr-10 text-gray-700"> Setting</h1>
+                        <h1 className="text-2xl font-montserrat font-semibold mt-32 ml-10 mr-10 text-gray-700"> Setting</h1>
                     </button>
                 </div>
-                <div className="absolute top-5 left-5 sm:top-24 sm:left-5 text-4xl font-bold">Dashboard</div>
+                <div className="absolute top-5 left-5 sm:top-24 sm:left-5 text-4xl font-palanquin font-bold">Dashboard</div>
             </div>
         </>
     )
