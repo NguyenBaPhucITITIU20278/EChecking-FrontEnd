f9865c656bd397ec7d2b34813e83d33907f6555d
Duy Nguyễn <nqd5758@gmail.com>
2024-05-11 23:27:51 +0700

    commit

---
+++ src/components/CourseComponents/SessionCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SessionCard = () => {
+    return (
+        <div className=' bg-white block bg-opacity-50 min-h-60 min-w-80 rounded-xl shadow-xl overflow'></div>
+    )
+}
+
+export default SessionCard
--- src/pages/CourseInfoPage.jsx
+++ src/pages/CourseInfoPage.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import CourseNavbar from '../components/CourseComponents/CourseNavbar'
 import BreadcrumbComponent from '../components/BreadcrumbComponent'
+import SessionCard from '../components/CourseComponents/SessionCard'
 
 const CourseInfoPage = () => {
     return (
-        <div className="h-full bg-gradient-to-tr from-slate-300 to-sky-300">
+        <div className="h-max bg-gradient-to-tr from-slate-300 to-sky-300">
             <CourseNavbar />
             <div className="p-4">
 
                 <BreadcrumbComponent />
-                <div>
-                    <h1 className='bg-white'>
-                        Course Information
-                    </h1>
-                    <h2>
+                <div id='course-info' className='px-6 pt-2 flex flex-col gap-4 '>
+                    <h1 className='font-poppins sm:text-4xl text-2xl'>
+                        <span>Course Name</span>
+                        <button>
 
-                    </h2>
+                        </button>
+                    </h1>
+                    <div className='bg-white p-4 rounded-lg shadow-xl h-max md:w-3/4 2xl:w-[1400px]'>
+                        <div className='text-justify font-palanquin sm:text-xl text-sm'>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        </div>
+                    </div>
+                </div>
+                <div id='sessions-container' className='pt-12 flex gap-8 flex-wrap'>
+                    <SessionCard />
                 </div>
             </div>
         </div>
