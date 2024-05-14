b553650d8e66deaaad994032ac4547de49537ac8
Duy Nguyễn <93112625+yudswin@users.noreply.github.com>
2024-05-14 15:24:02 +0700

    Course Modal

--- src/pages/CourseInfoPage.jsx
+++ src/pages/CourseInfoPage.jsx
@@ -2,12 +2,46 @@ import React, { useState } from 'react'
 import CourseNavbar from '../components/CourseComponents/CourseNavbar'
 import BreadcrumbComponent from '../components/BreadcrumbComponent'
 import SessionCard from '../components/CourseComponents/SessionCard'
-import { NotePencil } from '@phosphor-icons/react'
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
+import { Check, CheckFat, NotePencil, Pen, Pencil } from '@phosphor-icons/react'
+import { Modal, Form, Input } from 'antd'
 
 
 const CourseInfoPage = () => {
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isEdit, setIsEdit] = useState(false);
+    const [isEditInfo, setIsEditInfo] = useState(false);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    const showEdit = () => {
+        setIsEdit(true);
+        document.getElementById('courseNameInput').focus();
+        console.log(isEdit);
+    }
+
+    const handleUpdate = () => {
+        setIsEdit(false);
+    }
+
+    const showEditInfo = () => {
+        setIsEditInfo(true);
+        document.getElementById('courseDescriptionInput').focus();
+        console.log(isEdit);
+    }
+
+    const handleUpdateInfo = () => {
+        setIsEditInfo(false);
+    }
+
+
     return (
         <div className={`sm:h-full h-full bg-gradient-to-tr from-violet-400 to-sky-200`}>
             <CourseNavbar />
@@ -23,9 +57,9 @@ const CourseInfoPage = () => {
                         </div>
                     </div>
                     <div>
-                        <Button onPress={onOpen} className='absolute right-0 bottom-0 p-2 transition delay-100 '>
+                        <button onClick={showModal} className='absolute right-0 bottom-0 p-2 transition delay-100 '>
                             <NotePencil size={32} color='currentColor' className='transition hover:scale-110' />
-                        </Button>
+                        </button>
                     </div>
                 </div>
                 <div id='sessions-container' className='pt-12 flex gap-8 flex-wrap'>
@@ -41,41 +75,41 @@ const CourseInfoPage = () => {
                 </div>
             </div>
             <>
-                <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-                    <ModalContent>
-                        {(onClose) => (
-                            <>
-                                <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-                                <ModalBody>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                        Nullam pulvinar risus non risus hendrerit venenatis.
-                                        Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                                    </p>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                        Nullam pulvinar risus non risus hendrerit venenatis.
-                                        Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                                    </p>
-                                    <p>
-                                        Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
-                                        dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis.
-                                        Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod.
-                                        Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur
-                                        proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
-                                    </p>
-                                </ModalBody>
-                                <ModalFooter>
-                                    <Button color="danger" variant="light" onPress={onClose}>
-                                        Close
-                                    </Button>
-                                    <Button color="primary" onPress={onClose}>
-                                        Action
-                                    </Button>
-                                </ModalFooter>
-                            </>
-                        )}
-                    </ModalContent>
+                <Modal open={isModalOpen} okText="Done" onOk={handleOk} onCancel={handleCancel} footer={null} className='course-modal'>
+                    <legend className=' uppercase flex flex-col font-montserrat justify-center items-center'>
+                        <div className='text-xl'>Course Configuration</div>
+                        <div className='text-3xl'> COURSEID </div>
+                    </legend>
+                    <div id='courseName' className='flex flex-col mt-4'>
+                        <label htmlFor="courseName" className="block sm:text-lg text-sm font-montserrat leading-6 text-black">Class Name</label>
+                        <div className="flex relative">
+                            <input
+                                id="courseNameInput"
+                                name="courseName"
+                                placeholder='Name of the class'
+                                type="courseName"
+                                className={`font-palanquin block w-full rounded-md px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-blue-600 sm:text-sm sm:leading-6 ${isEdit ? "pointer-events-auto" : "pointer-events-none"}`}
+                            />
+                            <button onClick={showEdit} className={`${isEdit ? "hidden" : ""} absolute inset-y-0  right-2 transition delay-100 hover:scale-110`}>
+                                <NotePencil size={30} color='currentColor' className='' />
+                            </button>
+                            <button className={`${isEdit ? "" : "hidden"} absolute inset-y-0 right-2 transition delay-100 hover:scale-110`} onClick={handleUpdate}>
+                                <CheckFat size={30} color='#2b5432' weight='bold' className='' />
+                            </button>
+                        </div>
+                    </div>
+                    <div id='courseDescription' className='flex flex-col mt-4'>
+                        <label htmlFor="courseDescription" className="block sm:text-lg text-sm font-montserrat leading-6 text-black">Course Description</label>
+                        <div className="mt-2 relative">
+                            <input id="courseDescriptionInput" name="courseDescription" placeholder='Description of the course' type="text" className={`font-palanquin block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-blue-600 sm:text-sm sm:leading-6 ${isEditInfo ? "pointer-events-auto" : "pointer-events-none"}`} />
+                            <button onClick={showEditInfo} className={`${isEditInfo ? "hidden" : ""} absolute inset-y-0  right-2 transition delay-100 hover:scale-110`}>
+                                <NotePencil size={30} color='currentColor' className='' />
+                            </button>
+                            <button className={`${isEditInfo ? "" : "hidden"} absolute inset-y-0 right-2 transition delay-100 hover:scale-110`} onClick={handleUpdateInfo}>
+                                <CheckFat size={30} color='#2b5432' weight='bold' className='' />
+                            </button>
+                        </div>
+                    </div>
                 </Modal>
             </>
         </div>
