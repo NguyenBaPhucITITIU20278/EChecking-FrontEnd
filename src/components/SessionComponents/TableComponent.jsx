310f53dec013e696b29946d9d1af91a3d6ec943a
Duy Nguyễn <93112625+yudswin@users.noreply.github.com>
2024-05-26 17:40:21 +0700

    Update TableComponent.jsx

--- src/components/SessionComponents/TableComponent.jsx
+++ src/components/SessionComponents/TableComponent.jsx
@@ -36,89 +36,18 @@ const TableComponent = () => {
         },
     ];
 
-    const data = [
-        {
-            key: '1',
-            index: 1,
-            studentId: 'S101',
+    const data = [];
+    for (let i = 0; i < 30; i++) {
+        data.push({
+            key: i,
+            index: i,
+            studentId: 'S101' + i,
             studentName: 'John Doe',
-            joinAt: '2022-01-01',
-            fileUpload: 'https://example.com/file1.pdf',
-        },
-        {
-            key: '2',
-            index: 2,
-            studentId: 'S102',
-            studentName: 'Jane Smith',
-            joinAt: '2022-01-02',
-            fileUpload: 'https://example.com/file2.pdf',
-        },
-        {
-            key: '3',
-            index: 3,
-            studentId: 'S103',
-            studentName: 'Jim Brown',
-            joinAt: '2022-01-03',
-            fileUpload: 'https://example.com/file3.pdf',
-        },
+            joinAt: '2022-01-' + i,
+            fileUpload: 'https://example.com/file' + i,
+        });
+    }
 
-        {
-            key: "4",
-            index: 4,
-            studentId: "S104",
-            studentName: "Olivia Garcia",
-            joinAt: "2022-04-20",
-            fileUpload: "[invalid URL removed]"
-        },
-        {
-            key: "5",
-            index: 5,
-            studentId: "S105",
-            studentName: "William Harris",
-            joinAt: "2022-05-10",
-            fileUpload: "[invalid URL removed]"
-        },
-        {
-            key: "6",
-            index: 6,
-            studentId: "S106",
-            studentName: "Sophia Lee",
-            joinAt: "2022-06-01",
-            fileUpload: "[invalid URL removed]"
-        },
-        {
-            key: "7",
-            index: 7,
-            studentName: "Benjamin Jones",
-            studentId: "S107",
-            joinAt: "2022-07-15",
-            fileUpload: "[invalid URL removed]"
-        },
-        {
-            key: "8",
-            index: 8,
-            studentId: "S108",
-            studentName: "Emily Miller",
-            joinAt: "2022-08-08",
-            fileUpload: "[invalid URL removed]"
-        },
-        {
-            key: "9",
-            index: 9,
-            studentId: "S109",
-            studentName: "David Davis",
-            joinAt: "2022-09-02",
-            fileUpload: "[invalid URL removed]"
-        },
-        {
-            key: "10",
-            index: 10,
-            studentId: "S110",
-            studentName: "Isabella Garcia",
-            joinAt: "2022-10-16",
-            fileUpload: "[invalid URL removed]"
-        }
-    ];
 
     const rowSelection = {
         onChange: (selectedRowKeys, selectedRows) => {
