53c8489c06daccf5b20b58e7bb5fd7f29f79015d
Duy Nguyễn <nqd5758@gmail.com>
2024-06-03 02:27:34 +0700

    Update QuizPage.jsx

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Download, DownloadSimple, TrayArrowUp } from '@phosphor-icons/react'
+import { Download, DownloadSimple, TrayArrowUp, X } from '@phosphor-icons/react'
 import BackgroundImage from '../assets/BGROUND.jpg'; // Import the background image
 
 
@@ -15,14 +15,26 @@ const QuizPage = () => {
 
     const handleFileChange = (e) => {
         const files = e.target.files?.[0];
+        const fileNameElement = document.getElementById('fileNameElement');
         if (files) {
+            if (fileNameElement) {
+                fileNameElement.innerText = '';
+            }
             setFile(files);
         }
     };
 
+    const removeFile = () => {
+        const fileNameElement = document.getElementById('fileNameElement');
+        if (fileNameElement) {
+            fileNameElement.innerText = 'Browse your file here';
+        }
+        setFile(null);
+    };
+
     return (
         <div className='flex justify-center items-center sm: h-full h-svg w-full flex-col bg-white bg-opacity-40 sm:bg-opacity-10'>
-            <div className={`bg-white bg-opacity-40 w-[80%] h-[80%] rounded-2xl p-6`}>
+            <div className={`bg-white bg-opacity-40 w-[80%] h-max rounded-2xl p-6`}>
                 <div className='flex flex-col justify-center items-center p-4'>
                     <div className='font-poppins text-2xl normal-case drop-shadow-2xl font-black'>
                         Session Code
@@ -36,32 +48,28 @@ const QuizPage = () => {
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen bookorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. orem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. orem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                     </div>
                 </div>
-                <div className='flex sm:flex-row gap-4 justify-end'>
-                    <div>
-                        <label
-                            htmlFor="dropzone-file"
-                            className="flex flex-col items-center justify-center w-full h-64 bg-white border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-gray-300"
-                        >
-                            <input
-                                id="dropzone-file"
-                                type="file"
-                                className="hidden"
-                                onChange={handleFileChange}
-                            />
-                            <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                <p className="mb-2 text-sm text-gray-500">
+                <div className='flex sm:flex-row gap-4 justify-end p-4'>
+                    <div className='flex flex-row items-center'>
+                        <label htmlFor="dropzone-file" className="flex flex-row items-center gap-4 border border-dashed p-4 border-black ">
+                            <input id="dropzone-file" type="file" className="hidden" onChange={handleFileChange} />
+                            <div className="flex flex-col items-center justify-center">
+                                <span className="text-sm text-gray-500">
                                     <TrayArrowUp size={32} />
-                                </p>
+                                </span>
                             </div>
-                            <p
-                                id="fileNameElement"
-                                className="mb-2 text-sm text-gray-500"
-                            ></p>
+                            <div id="fileNameElement" className="text-sm text-gray-500">
+                                Browse your file here
+                            </div>
+                            <button onClick={removeFile} className='border border-black p-2 rounded-lg'>
+                                <X size={16} />
+                            </button>
                         </label>
                     </div>
-                    <div>
-                        Submit Button
-                    </div>
+                    <button className='border border-black w-1/5 rounded-xl p-2 bg-white hover:bg-blue-400 group'>
+                        <span className='text-lg uppercase font-montserrat text-gray-500 font-black tracking-wider group-hover:text-white'>
+                            Submit
+                        </span>
+                    </button>
                 </div>
             </div>
         </div>
