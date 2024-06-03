9a1fa857808ceff5506d1f90057bf92ef72bae70
Duy Nguyễn <93112625+yudswin@users.noreply.github.com>
2024-06-03 13:50:02 +0700

    Update Responsive QuizPage
    _ responsive handle for above Iphone XR

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -5,21 +5,21 @@ import BackgroundImage from '../assets/BGROUND.jpg'; // Import the background im
 
 const QuizPage = () => {
     const [file, setFile] = useState(null);
+    const [fileName, setFileName] = useState('');
+
 
     useEffect(() => {
-        const fileNameElement = document.getElementById('fileNameElement');
-        if (file && fileNameElement) {
-            fileNameElement.innerText = file.name;
+        if (file) {
+            const name = file.name;
+            const extension = name.split('.').pop();
+            const truncatedName = name.length > 15 ? name.substring(0, 15) + '...' : name;
+            setFileName(`${truncatedName}.${extension}`);
         }
     }, [file]);
 
     const handleFileChange = (e) => {
         const files = e.target.files?.[0];
-        const fileNameElement = document.getElementById('fileNameElement');
         if (files) {
-            if (fileNameElement) {
-                fileNameElement.innerText = '';
-            }
             setFile(files);
         }
     };
@@ -30,43 +30,45 @@ const QuizPage = () => {
             fileNameElement.innerText = 'Browse your file here';
         }
         setFile(null);
+        console.log(file);
     };
 
     return (
-        <div className='flex justify-center items-center sm: h-full h-svg w-full flex-col bg-white bg-opacity-40 sm:bg-opacity-10'>
-            <div className={`bg-white bg-opacity-40 w-[80%] h-max rounded-2xl p-6`}>
-                <div className='flex flex-col justify-center items-center p-4'>
-                    <div className='font-poppins text-2xl normal-case drop-shadow-2xl font-black'>
+        <div className='flex justify-center sm:items-center h-max sm:w-full flex-col sm:py-0 py-2'>
+            <div className={`sm:bg-white sm:bg-opacity-40 sm:w-[80%] sm:h-max rounded-2xl p-6`}>
+                <div className='flex flex-col sm:justify-center items-center p-4'>
+                    <div className='font-poppins sm:text-2xl text-xl normal-case drop-shadow-2xl font-black'>
                         Session Code
                     </div>
-                    <div className='sm:flex hidden font-poppins text-4xl normal-case drop-shadow-2xl text-white font-black text-stroke-black'>
+                    <div className='sm:flex font-poppins sm:text-4xl text-2xl normal-case drop-shadow-2xl text-white font-black text-stroke-black'>
                         Course Name
                     </div>
                 </div>
-                <div className='flex p-6 bg-white h-2/3 rounded-xl'>
-                    <div className='text-3xl font-poppins text-justify text-ellipsis overflow-scroll'>
+                <div className='flex p-6 bg-white sm:h-2/3 min-h-1/3 max-h-[400px] rounded-xl'>
+                    <div className='sm:text-3xl text-base font-poppins text-justify text-ellipsis overflow-scroll'>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen bookorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. orem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. orem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                     </div>
                 </div>
                 <div className='flex sm:flex-row gap-4 justify-end p-4'>
-                    <div className='flex flex-row items-center'>
-                        <label htmlFor="dropzone-file" className="flex flex-row items-center gap-4 border border-dashed p-4 border-black ">
+                    <div className='flex flex-row items-center sm:w-1/2 w-max'>
+                        <label htmlFor="dropzone-file" className="w-full group flex flex-row justify-between items-center gap-4 sm:border-2 rounded-xl hover:bg-blue-500 border hover:border-solid transition ease-in-out duration-500  border-dashed p-4 border-black ">
                             <input id="dropzone-file" type="file" className="hidden" onChange={handleFileChange} />
-                            <div className="flex flex-col items-center justify-center">
-                                <span className="text-sm text-gray-500">
+                            <div className="flex flex-row gap-4 items-center justify-center">
+                                <div className={`${file === null ? 'flex' : 'hidden'} text-sm sm:text-gray-500 text-black group-hover:text-white`}>
                                     <TrayArrowUp size={32} />
-                                </span>
+                                </div>
+                                <div className={`w-1/2 flex flex-row overflow-hidden text-xs sm:text-gray-500 text-black group-hover:text-white font-poppins transition ${file ? 'sm:overflow-visible overflow-ellipsis' : 'sm:overflow-visible overflow-hidden'} whitespace-nowrap`}>
+                                    {file ? (window.innerWidth > 640 ? file.name : fileName) : 'Browse your file here'}
+                                </div>
                             </div>
-                            <div id="fileNameElement" className="text-sm text-gray-500">
-                                Browse your file here
-                            </div>
-                            <button onClick={removeFile} className='border border-black p-2 rounded-lg'>
-                                <X size={16} />
+
+                            <button onClick={removeFile} className={`${file === null ? 'hidden' : 'flex'} border group-hover:border-white text-[#ed3833] border-[#ed3833] p-2 rounded-full group hover:scale-110`}>
+                                <X size={16} className='group-hover:text-white' />
                             </button>
                         </label>
                     </div>
-                    <button className='border border-black w-1/5 rounded-xl p-2 bg-white hover:bg-blue-400 group'>
-                        <span className='text-lg uppercase font-montserrat text-gray-500 font-black tracking-wider group-hover:text-white'>
+                    <button className='border border-black sm:w-1/5  rounded-xl p-2  bg-white transition hover:bg-blue-500 ease-in-out duration-500 group'>
+                        <span className='sm:text-lg text-xs uppercase font-montserrat sm:text-gray-500 text-black font-black tracking-wider group-hover:text-white'>
                             Submit
                         </span>
                     </button>
